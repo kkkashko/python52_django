6951f29f98a780c5c3b1bf8599e78e3172083060
kkkashko <vanya6kashko@gmail.com>
2026-06-24 20:14:09 +0700

    первая пара третьего занятия. шаблоны

--- my_site/urls.py
+++ my_site/urls.py
@@ -2,25 +2,9 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from shop import views #Поключение представления
 
-product_lists = [ 
-    path('', views.product),
-    path('new-product', views.new_products),
-    path('top-product', views.top_products),
-]
-
 urlpatterns = [ #Указание маршрутов
     path('admin/', admin.site.urls),
     path('', views.index),
-    re_path(r'^about/contact', 
-            views.contact, 
-            kwargs={
-                'name': 'Bob', 
-                'age': 40, 'email': 
-                'bob@gmail.com'
-            }),
-    re_path(r'^about', views.about),
-    path('brw-info', views.browser_info),
-    path('user/<str:name>', views.user),
-    path('user/<str:name>/<int:age>', views.user),
-    path('products/<int:id>/', include(product_lists)),
+    path('about/', views.about),
+    path('contact/', views.contact),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,33 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse #Для работы с запросами
 
 def index(request):
-    return HttpResponse("<h1>Главная страница</h1>")
+    header = 'hello, user'
+    array = [1, 2, 3, 4, 5]
+    user = {'name': 'Bob', 'age': 23}
+    address = ('Ленина', 2, 54, 23)
+
+    data = {
+        'header': header,
+        'array': array,
+        'user': user,
+        'address': address
+    }
+
+    return render(request, "shop/index.html", context=data)
 
 def about(request):
-    return HttpResponse("<h1>О нас</h1>")
+    return render(request, 'shop/about.html')
 
-def contact(request, name: str, age: int, email: str):
-    return HttpResponse(f"""
-        <h2>Name: {name}</h2>
-        <h2>Age: {age}</h2>
-        <h2>Email: {email}</h2>
-    """)
-
-def browser_info(request):
-    return HttpResponse(f"""
-        <p>Схема запроса: {request.scheme}</p>
-        <p>Вес тела (байт): {request.body}</p>
-        <p>IP-адрес клиента: {request.META['REMOTE_ADDR']}</p>
-    """)
-
-def user(request, name="Undefined", age=0):
-    return HttpResponse(f"Username: {name} Age: {age}")
-
-def product(request, id):
-    return HttpResponse(f"Список товаров, id: {id}")
-
-def new_products(requests, id):
-    return HttpResponse(f"Новые товары, id: {id}")
-
-def top_products(requests, id):
-    return HttpResponse(f"Популярные продукты, id: {id}")
+def contact(request):
+    return render(request, 'shop/contact.html')
