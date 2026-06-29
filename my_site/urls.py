5a39f3a793aad6da5c733f04268dac621e5f272b
kkkashko <vanya6kashko@gmail.com>
2026-06-29 21:36:17 +0700

    работа с шаблонами и статикой

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from shop import views #Поключение представления
 
 urlpatterns = [ #Указание маршрутов
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('about/', views.about),
-    path('contact/', views.contact),
+    path('about/', TemplateView.as_view(template_name="shop/about.html", 
+        extra_context={"header": "Подробная информация"})),
+    path('contact/', TemplateView.as_view(template_name="shop/contact.html")),
 ]
